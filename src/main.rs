74ecc83d8edb518867d8ff697b49856abc8e7977
Alex Pojman <pojman.alex@gmail.com>
2023-01-11 22:07:09 -0600

    Optimize dockerfile

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use newsletter::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -11,9 +11,11 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres");
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
